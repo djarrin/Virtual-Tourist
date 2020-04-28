9297501bee60c6611d0091aa8486bdad56fedf26
David Jarrin <dmjarrin@gmail.com>
2020-04-28 17:47:19 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by David Jarrin on 4/28/20.
+//  Copyright © 2020 David Jarrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
