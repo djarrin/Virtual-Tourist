5c4ea43a78c1289ec7f77acc48e0a42817134f8b
David Jarrin <dmjarrin@gmail.com>
2020-05-01 08:52:41 -0400

    atleast have the basic project structure put together, request client set up to write calls and basic view structure laid out

--- Virtual Tourist/Controller/TravelMapViewController.swift
+++ Virtual Tourist/Controller/TravelMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelMapViewController.swift
 //  Virtual Tourist
 //
 //  Created by David Jarrin on 4/28/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
